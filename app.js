8f6ec61fec445477e6030a50b40292d20ec9cfa4
Mohammedzv <mf9391224@gmail.com>
2026-05-01 14:49:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,4 +7,36 @@ localStorage.setItem("username", username);
 
 window.location.href = "chat.html";
 
+const db = firebase.firestore();
+
+function sendMsg(){
+const msg = document.getElementById("msg").value;
+const user = localStorage.getItem("username");
+
+db.collection("messages").add({
+text: msg,
+user: user,
+time: Date.now()
+});
+
+document.getElementById("msg").value = "";
+}
+
+db.collection("messages").orderBy("time")
+.onSnapshot(snapshot => {
+
+document.getElementById("messages").innerHTML = "";
+
+snapshot.forEach(doc => {
+let data = doc.data();
+
+document.getElementById("messages").innerHTML += `
+<div class="msg">
+<b>${data.user}</b><br>
+${data.text}
+</div>
+`;
+});
+
+});
 }
